154afe0553d6a135e4c22a58d1ffbff5793cb8c7
Antonino Praxedes <antonino.praxedes@gmail.com>
2021-01-29 20:30:51 -0400

    feat: adding avatar icons

---
+++ lib/data/dummy.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+      id: '1',
+      name: 'John',
+      email: 'John@test.com',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2016/11/18/23/38/child-1837375_960_720.png'),
+  '2': const User(
+      id: '2',
+      name: 'Mikey',
+      email: 'Mikey@test.com',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2016/03/31/20/27/avatar-1295773_960_720.png'),
+  '3': const User(
+      id: '3',
+      name: 'Rosy',
+      email: 'rosy@test.com',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2021/01/24/09/28/girl-5944691_960_720.jpg'),
+  '4': const User(
+      id: '4',
+      name: 'Antonino',
+      email: 'rosy@test.com',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2016/09/01/08/24/smiley-1635449_960_720.png'),
+};
